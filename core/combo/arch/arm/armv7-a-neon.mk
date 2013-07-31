94603f2737a92eb4e5dd4a709ec3885571e46294
AndroidMeda <hephappy@gmail.com>
2013-07-31 16:41:50 +0300

    Re-add ARCH_ARM_HAVE_TLS_REGISTER for armv7
    OpenGL needs this flag for armv7
    
    For more information :
    https://github.com/androidarmv6/android_bionic/commit/7bc88df02f887229738b08fd7f7bd2668805f306
    https://github.com/androidarmv6/android_frameworks_native/commit/4ffa795a3e9f2d2a26766147b1cfedbe78e823d4
    
    Change-Id: Ib68c17e2f8dd3c8c15f8f3ea235fe25a46d05b6a

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -2,6 +2,7 @@
 # Generating binaries for the ARMv7-a architecture and higher with NEON
 #
 ARCH_ARM_HAVE_ARMV7A            := true
+ARCH_ARM_HAVE_TLS_REGISTER      := true
 ARCH_ARM_HAVE_VFP               := true
 ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
