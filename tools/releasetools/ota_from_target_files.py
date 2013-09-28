3436b646959868d13488bf531184ee649aa6c24d
Andrew Dodd <atd7@cornell.edu>
2013-09-28 15:24:20 -0400

    Add backuptool support to the releasetools system
    Pulled from CyanogenMod 10.1
    
     Conflicts:
    	tools/releasetools/ota_from_target_files.py
    
    Change-Id: Iffd9a45865011ad9108508d31f79e53d609dd6f4

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -124,6 +124,10 @@ Usage:  ota_from_target_files [flags] input_target_files output_ota_package
 
   --payload_signer_args <args>
       Specify the arguments needed for payload signer.
+
+  --backup <boolean>
+      Enable or disable the execution of backuptool.sh.
+      Disabled by default.
 """
 
 import sys
@@ -174,6 +178,7 @@ OPTIONS.gen_verify = False
 OPTIONS.log_diff = None
 OPTIONS.payload_signer = None
 OPTIONS.payload_signer_args = []
+OPTIONS.backuptool = False
 
 def MostPopularKey(d, default):
   """Given a dict, return the key corresponding to the largest
@@ -629,6 +634,11 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   device_specific.FullOTA_InstallBegin()
 
+  if OPTIONS.backuptool:
+    script.Mount("/system")
+    script.RunBackup("backup")
+    script.Unmount("/system")
+
   system_progress = 0.75
 
   if OPTIONS.wipe_user_data:
@@ -675,6 +685,10 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
 #    common.MakeRecoveryPatch(OPTIONS.input_tmp, output_sink,
 #                             recovery_img, boot_img)
+
+  if OPTIONS.backuptool:
+    script.ShowProgress(0.2, 10)
+    script.RunBackup("restore")
 
     system_items.GetMetadata(input_zip)
     system_items.Get("system").SetPermissions(script)
@@ -1949,6 +1963,8 @@ def main(argv):
       OPTIONS.payload_signer = a
     elif o == "--payload_signer_args":
       OPTIONS.payload_signer_args = shlex.split(a)
+    elif o in ("--backup"):
+      OPTIONS.backuptool = bool(a.lower() == 'true')
     else:
       return False
     return True
@@ -1980,6 +1996,7 @@ def main(argv):
                                  "log_diff=",
                                  "payload_signer=",
                                  "payload_signer_args=",
+                                 "backup=",
                              ], extra_option_handler=option_handler)
 
   if len(args) != 2:
