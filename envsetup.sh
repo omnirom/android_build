bcf5d754f890266729ee8b78b73e97c4a343d445
Andrew Dodd <atd7@cornell.edu>
2013-09-28 15:26:50 -0400

    envsetup: Set CUSTOM_BUILD environment variable
    This is used to gate a few features, including backuptool
    
    Change-Id: I02fee9e0765cca88c833bb0514762aa3b14642e3

--- envsetup.sh
+++ envsetup.sh
@@ -144,6 +144,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^custom_") ; then
+       CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^custom_//g')
+    else
+       CUSTOM_BUILD=
+    fi
+    export CUSTOM_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
