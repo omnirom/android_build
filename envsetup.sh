15a67f119e18069262870948d5f1d0f87c964048
Pulser <pulser@xda-developers.com>
2013-09-29 09:56:55 +0100

    Add repopick script from CM
    Change-Id: If67c5f178d03fadd8b79b1908f4a9cb231efbb28

--- envsetup.sh
+++ envsetup.sh
@@ -1325,6 +1325,12 @@ function set_java_home() {
     fi
 }
 
+function repopick() {
+    T=$(gettop)
+    $T/build/tools/repopick.py $@
+}
+
+
 # Print colored exit condition
 function pez {
     "$@"
