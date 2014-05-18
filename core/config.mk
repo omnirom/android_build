033e325650036e97c5e1d240f3f949931d71906f
Steve Kondik <shade@chemlab.org>
2014-05-18 15:34:30 -0700

    build: Include vendor/extra/BoardConfigExtra.mk if available
    Change-Id: Icd0db0852ceee5cb5c701c9a40b829e1f3ebd69e

--- core/config.mk
+++ core/config.mk
@@ -358,6 +358,8 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 include vendor/omni/config/BoardConfigOmni.mk
 
+-include vendor/extra/BoardConfigExtra.mk
+
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
