521b0e492fb72da35d188eed079f988086f419b6
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-06-15 12:55:36 +0700

    [WIP] fix DSPmanager on falcon xt1032
    Change-Id: I504c7c4e05a7a27eb2d719193b37c177ce3466e5

--- target/product/generic_no_telephony.mk
+++ target/product/generic_no_telephony.mk
@@ -75,8 +75,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     local_time.default
 
-PRODUCT_COPY_FILES := \
-        frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf
+#PRODUCT_COPY_FILES := \
+#        frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.carrier=unknown
