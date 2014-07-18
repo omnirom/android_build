0ee396e25ad58ae12c10392fff5041282c71f136
Jiangyi <sam.andrew.jiang@gmail.com>
2014-07-18 15:53:13 -0400

    build: Add chromium prebuilt support to envsetup.sh && The core Makefile
    This adds a chromium_prebuilt function to envsetup.sh that is invoked by lunch to check
    whether the chromium prebuilts are up-to-date or not. If not, it will be built from source
    and then the new source built version will be pulled during brunch/mka bacon to become the
    new prebuilts for future builds.
    
    This is all opt-in through the USE_CHROMIUM_PREBUILT flag. Without it being set to 1,
    none of this would be ran, and regular operations will go on.
    
    Change-Id: I40f8c8dbd2a8a84a5c1b9f47ee04180a71ef4e07

--- envsetup.sh
+++ envsetup.sh
@@ -589,10 +589,15 @@ function lunch()
 
     echo
 
+    if [[ $USE_PREBUILT_CHROMIUM -eq 1 ]]; then
+        chromium_prebuilt
+    fi
+
     fixup_common_out_dir
 
     set_stuff_for_environment
     printconfig
+
 }
 
 # Tab completion for lunch.
@@ -1444,6 +1449,20 @@ function pez {
         echo -e "\e[0;32mSUCCESS\e[00m"
     fi
     return $retval
+}
+
+function chromium_prebuilt() {
+    T=$(gettop)
+    TARGET_DEVICE=$(get_build_var TARGET_DEVICE)
+    hash=$T/prebuilts/chromium/$TARGET_DEVICE/hash.txt
+
+    if [ -r $hash ] && [ $(git -C $T/external/chromium rev-parse --verify HEAD) == $(cat $hash) ]; then
+        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=yes
+        echo "** Prebuilt Chromium is up-to-date; Will be used for build **"
+    else
+        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=no
+        echo "** Prebuilt Chromium out-of-date/not found; Will build from source **"
+    fi
 }
 
 if [ "x$SHELL" != "x/bin/bash" ]; then
