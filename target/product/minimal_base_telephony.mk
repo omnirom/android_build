460f6ab9fb07a8cc479571d6194771845afee5b6
Humberto Borba <humberos@gmail.com>
2015-02-11 21:30:00 -0200

    build: product: add new target for low memory devices
    remove all live wallpapers
    
    just replace:
        $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
    
    to:
        $(call inherit-product, $(SRC_TARGET_DIR)/product/minimal_base_telephony.mk)
    
    In general this entry is located on device repo, in omni_[device name].mk
    Make sure that not exists LiveWallpapers declarations on device vendor repo.
    
    reference:
    https://source.android.com/devices/tech/low-ram.html
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ib77c3e2d13cc43981a0e12d853346abfc7549c67

---
+++ target/product/minimal_base_telephony.mk
@@ -0,0 +1,69 @@
+#
+# Copyright (C) 2015 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This is a build configuration for a minimal-featured build of the
+# Open-Source part of the tree. It's geared toward a US-centric
+# build quite specifically for the emulator, and might not be
+# entirely appropriate to inherit from for on-device configurations.
+#
+# This should be used for low memory devices
+
+PRODUCT_PACKAGES := \
+    VoiceDialer \
+    libsrec_jni
+
+PRODUCT_PACKAGES := \
+    libfwdlockengine \
+    VideoEditor \
+    OpenWnn \
+    libWnnEngDic \
+    libWnnJpnDic \
+    libwnndict \
+    WAPPushManager
+
+# Additional settings used in all AOSP builds
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.config.alarm_alert=Argon.ogg \
+    ro.config.ringtone=Orion.ogg \
+    ro.config.notification_sound=pixiedust.ogg
+
+# Put en_US first in the list, so make it default.
+PRODUCT_LOCALES := en_US
+
+# Include drawables for all densities
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+
+# Get some sounds
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+
+# Get the TTS language packs
+$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
+
+# Get a list of languages.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
+
+# Get everything else from the parent package
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    keyguard.no_require_sim=true \
+    ro.com.android.dataroaming=true
+
+PRODUCT_COPY_FILES := \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
