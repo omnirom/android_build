4ea33032c092a507f2702f98beef56cd7addb04c
sainath <sainathprasanna@gmail.com>
2016-05-05 19:45:43 +0530

    build: Change the cache partition size check into warnings.
    Change-Id: I89b81a4f1767b875b5964e2890b7cad33a6f913f

--- tools/releasetools/blockimgdiff.py
+++ tools/releasetools/blockimgdiff.py
@@ -483,20 +483,23 @@ class BlockImageDiff(object):
       if free_string:
         out.append("".join(free_string))
 
-      if self.version >= 2:
-        # Sanity check: abort if we're going to need more stash space than
-        # the allowed size (cache_size * threshold). There are two purposes
-        # of having a threshold here. a) Part of the cache may have been
-        # occupied by some recovery logs. b) It will buy us some time to deal
-        # with the oversize issue.
-        cache_size = common.OPTIONS.cache_size
-        stash_threshold = common.OPTIONS.stash_threshold
-        max_allowed = cache_size * stash_threshold
-        assert max_stashed_blocks * self.tgt.blocksize < max_allowed, \
-               'Stash size %d (%d * %d) exceeds the limit %d (%d * %.2f)' % (
-                   max_stashed_blocks * self.tgt.blocksize, max_stashed_blocks,
-                   self.tgt.blocksize, max_allowed, cache_size,
-                   stash_threshold)
+        # sanity check: abort if we're going to need more than 512 MB if
+        # stash space
+        assert max_stashed_blocks * self.tgt.blocksize < (512 << 20)
+        if self.version >= 2 and common.OPTIONS.cache_size is not None:
+         # Sanity check: abort if we're going to need more stash space than
+         # the allowed size (cache_size * threshold). There are two purposes
+         # of having a threshold here. a) Part of the cache may have been
+         # occupied by some recovery logs. b) It will buy us some time to deal
+         # with the oversize issue.
+         cache_size = common.OPTIONS.cache_size
+         stash_threshold = common.OPTIONS.stash_threshold
+         max_allowed = cache_size * stash_threshold
+         assert max_stashed_blocks * self.tgt.blocksize < max_allowed, \
+                'Stash size %d (%d * %d) exceeds the limit %d (%d * %.2f)' % (
+                    max_stashed_blocks * self.tgt.blocksize, max_stashed_blocks,
+                    self.tgt.blocksize, max_allowed, cache_size,
+                    stash_threshold)
 
     # Zero out extended blocks as a workaround for bug 20881595.
     if self.tgt.extended:
@@ -523,11 +526,19 @@ class BlockImageDiff(object):
         f.write(i)
 
     if self.version >= 2:
+      print("max stashed blocks: %d (%d bytes)\n" % (
+          max_stashed_blocks, max_stashed_blocks * self.tgt.blocksize))
       max_stashed_size = max_stashed_blocks * self.tgt.blocksize
-      max_allowed = common.OPTIONS.cache_size * common.OPTIONS.stash_threshold
-      print("max stashed blocks: %d  (%d bytes), limit: %d bytes (%.2f%%)\n" % (
-          max_stashed_blocks, max_stashed_size, max_allowed,
-          max_stashed_size * 100.0 / max_allowed))
+      OPTIONS = common.OPTIONS
+      if OPTIONS.cache_size is not None:
+        max_allowed = OPTIONS.cache_size * OPTIONS.stash_threshold
+        print("max stashed blocks: %d (%d bytes), "
+              "limit: %d bytes (%.2f%%)\n" % (
+              max_stashed_blocks, max_stashed_size, max_allowed,
+              max_stashed_size * 100.0 / max_allowed))
+      else:
+        print("max stashed blocks: %d (%d bytes), limit: <unknown>\n" % (
+              max_stashed_blocks, max_stashed_size))
 
   def ReviseStashSize(self):
     print("Revising stash size...")
