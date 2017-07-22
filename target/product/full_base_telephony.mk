e09b7399e25a2d79bb62747e7e483802150138c8
maxwen <max.weninger@gmail.com>
2017-07-22 23:00:19 +0200

    build: remove default ro.com.android.dataroaming=true
    Change-Id: I7aad16a6bcdb55b954ed78cf77f5aa3072a3d53c

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -20,8 +20,7 @@
 # entirely appropriate to inherit from for on-device configurations.
 
 PRODUCT_PROPERTY_OVERRIDES := \
-    keyguard.no_require_sim=true \
-    ro.com.android.dataroaming=true
+    keyguard.no_require_sim=true
 
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
