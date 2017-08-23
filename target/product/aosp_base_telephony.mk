1a064ff1c418e4468b5a33d2519301e4ce1cc408
Menno van Grinsven <scanno@scanno.nl>
2017-08-23 16:00:15 +0200

    Revert "base: move Telecomn TeleService and MmsService into telephony only"
    This reverts commit bc1e876d0c812dc719ffec234bd020d9caaf6fc9.
    
    Change-Id: Ic044312bc1f90139323ef5c0075299dfd523e44f

--- target/product/aosp_base_telephony.mk
+++ target/product/aosp_base_telephony.mk
@@ -16,9 +16,4 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 PRODUCT_PACKAGES += \
-    Telecom \
-    TeleService \
-    MmsService
-
-PRODUCT_PACKAGES += \
     messaging
