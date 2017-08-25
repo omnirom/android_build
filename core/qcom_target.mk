5726c9d4ea1af88746edc4337f2a4e56d6a78320
Marko Man <darkobas@gmail.com>
2017-08-25 13:58:36 +0200

    qcom_target: change global flag names
    Change-Id: I4e8e61ebb276765c4400dc60a21f63e0dfeb8c86

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -6,16 +6,16 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
         qcom_flags += -DQTI_BSP
     endif
 
-    TARGET_GLOBAL_CFLAGS += $(qcom_flags)
-    TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
-    CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
-    CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+#    TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+#    TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+    PRIVATE_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    PRIVATE_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
     # Multiarch needs these too..
-    2ND_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
-    2ND_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
-    2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
-    2ND_CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+#    2ND_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+#    2ND_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+#    2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+#    2ND_CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
     TARGET_COMPILE_WITH_MSM_KERNEL := true
     MSM_VIDC_TARGET_LIST := msm8974 msm8610 msm8226 apq8084 msm8916 msm8937 msm8952 msm8953 msm8994 msm8909 msm8992 msm8996 msm8998
