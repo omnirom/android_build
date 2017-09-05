c38d5d691622d408630362dd0bfbcc7dc738a49f
maxwen <max.weninger@gmail.com>
2017-09-05 17:56:35 +0200

    build: update default app list
    disable old deprecated apps
    
    Change-Id: Iffa963505eb5be5064c436ff02a599ad74bed27b

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -21,7 +21,7 @@
 
 PRODUCT_PROPERTY_OVERRIDES := \
     keyguard.no_require_sim=true \
-    ro.com.android.dataroaming=true
+    ro.com.android.dataroaming=false
 
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
