724df93e460dc50ce43da0bc7bcccd2125fdb2d3
Humberto Borba <humberos@gmail.com>
2017-09-08 10:53:08 -0300

    [DO NOT MERGE] build: enable adb early
    Change-Id: I43e7b1f82e8ad843636263fa354ca937d6fc3a87
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -274,10 +274,8 @@ ifneq (,$(user_variant))
   # Target is secure in user builds.
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=0
-
-  ifeq ($(user_variant),user)
-    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
-  endif
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+  ADDITIONAL_DEFAULT_PROPERTIES += persist.sys.usb.config=mtp,adb
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
