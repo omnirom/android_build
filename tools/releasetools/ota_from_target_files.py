f91f5177caddb7afe0f1b1fbf94b12d261f0bc43
maxwen <max.weninger@gmail.com>
2018-02-26 00:27:39 +0100

    build: make using brotli configurable
    set ANDROID_COMPILE_WITH_BROTLI env variable to
    true (default) or false to enable (default) or
    disable usage of brotli
    
    Change-Id: If95145c70f8700502fce2f7f84342cdd929fcdba

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -163,6 +163,10 @@ Usage:  ota_from_target_files [flags] input_target_files output_ota_package
   --backup <boolean>
       Enable or disable the execution of backuptool.sh.
       Disabled by default.
+
+  --brotli <boolean>
+     Enable (default) or disable usage of brotli
+
 """
 
 from __future__ import print_function
@@ -216,7 +220,7 @@ OPTIONS.key_passwords = []
 OPTIONS.skip_postinstall = False
 OPTIONS.override_device = 'auto'
 OPTIONS.backuptool = False
-
+OPTIONS.brotli = True
 
 METADATA_NAME = 'META-INF/com/android/metadata'
 POSTINSTALL_CONFIG = 'META/postinstall_config.txt'
@@ -850,7 +854,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   system_tgt = common.GetSparseImage("system", OPTIONS.input_tmp, input_zip,
                                      allow_shared_blocks)
   system_tgt.ResetFileMap()
-  system_diff = common.BlockDifference("system", system_tgt, src=None)
+  system_diff = common.BlockDifference("system", system_tgt, src=None, brotli=OPTIONS.brotli)
   system_diff.WriteScript(script, output_zip)
 
   boot_img = common.GetBootableImage(
@@ -862,7 +866,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     vendor_tgt = common.GetSparseImage("vendor", OPTIONS.input_tmp, input_zip,
                                        allow_shared_blocks)
     vendor_tgt.ResetFileMap()
-    vendor_diff = common.BlockDifference("vendor", vendor_tgt)
+    vendor_diff = common.BlockDifference("vendor", vendor_tgt, brotli=OPTIONS.brotli)
     vendor_diff.WriteScript(script, output_zip)
 
   AddCompatibilityArchiveIfTrebleEnabled(input_zip, output_zip, target_info)
@@ -1432,7 +1436,8 @@ def WriteBlockIncrementalOTAPackage(target_zip, source_zip, output_file):
   system_diff = common.BlockDifference("system", system_tgt, system_src,
                                        check_first_block,
                                        version=blockimgdiff_version,
-                                       disable_imgdiff=disable_imgdiff)
+                                       disable_imgdiff=disable_imgdiff,
+                                       brotli=OPTIONS.brotli)
 
   if HasVendorPartition(target_zip):
     if not HasVendorPartition(source_zip):
@@ -1450,7 +1455,8 @@ def WriteBlockIncrementalOTAPackage(target_zip, source_zip, output_file):
     vendor_diff = common.BlockDifference("vendor", vendor_tgt, vendor_src,
                                          check_first_block,
                                          version=blockimgdiff_version,
-                                         disable_imgdiff=disable_imgdiff)
+                                         disable_imgdiff=disable_imgdiff,
+                                         brotli=OPTIONS.brotli)
   else:
     vendor_diff = None
 
@@ -1871,6 +1877,8 @@ def main(argv):
       OPTIONS.override_device = a
     elif o in ("--backup"):
       OPTIONS.backuptool = bool(a.lower() == 'true')
+    elif o in ("--brotli"):
+      OPTIONS.brotli = bool(a.lower() == 'true')
     else:
       return False
     return True
@@ -1903,6 +1911,7 @@ def main(argv):
                                  "skip_postinstall",
                                  "override_device=",
                                  "backup=",
+                                 "brotli="
                              ], extra_option_handler=option_handler)
 
   if len(args) != 2:
