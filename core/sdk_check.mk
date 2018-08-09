68214f2745389a3d5b3baa1aeda461d3ecc9bdf6
Marko Man <darkobas@gmail.com>
2018-08-09 11:54:59 +0200

    [TEMP] Disable Apps SDK checks
    Change-Id: Icbd25c28457ef15987999ab87719ee069fa7fdd4

--- core/sdk_check.mk
+++ core/sdk_check.mk
@@ -10,28 +10,28 @@ whitelisted_modules := framework-res__auto_generated_rro
 
 
 ifeq (,$(JAVA_SDK_ENFORCEMENT_ERROR))
-  JAVA_SDK_ENFORCEMENT_ERROR := APPS
-endif
-
-ifeq ($(LOCAL_SDK_VERSION)$(LOCAL_PRIVATE_PLATFORM_APIS),)
-  ifeq (,$(filter $(LOCAL_MODULE),$(whitelisted_modules)))
-    ifneq ($(JAVA_SDK_ENFORCEMENT_WARNING)$(JAVA_SDK_ENFORCEMENT_ERROR),)
-      my_message := Must specify LOCAL_SDK_VERSION or LOCAL_PRIVATE_PLATFORM_APIS,
-      ifeq ($(LOCAL_MODULE_CLASS),$(JAVA_SDK_ENFORCEMENT_ERROR))
-        $(call pretty-error,$(my_message))
-      endif
-      ifeq ($(LOCAL_MODULE_CLASS),$(JAVA_SDK_ENFORCEMENT_WARNING))
-        $(call pretty-warning,$(my_message))
-      endif
-      my_message :=
-    endif
-  endif
-else ifneq ($(LOCAL_SDK_VERSION),)
-  ifneq ($(LOCAL_PRIVATE_PLATFORM_APIS),)
-    my_message := Specifies both LOCAL_SDK_VERSION ($(LOCAL_SDK_VERSION)) and
-    my_message += LOCAL_PRIVATE_PLATFORM_APIS ($(LOCAL_PRIVATE_PLATFORM_APIS))
-    my_message += but should specify only one
-    $(call pretty-error,$(my_message))
-    my_message :=
-  endif
+#  JAVA_SDK_ENFORCEMENT_ERROR := APPS
+#endif
+#
+#ifeq ($(LOCAL_SDK_VERSION)$(LOCAL_PRIVATE_PLATFORM_APIS),)
+#  ifeq (,$(filter $(LOCAL_MODULE),$(whitelisted_modules)))
+#    ifneq ($(JAVA_SDK_ENFORCEMENT_WARNING)$(JAVA_SDK_ENFORCEMENT_ERROR),)
+#      my_message := Must specify LOCAL_SDK_VERSION or LOCAL_PRIVATE_PLATFORM_APIS,
+#      ifeq ($(LOCAL_MODULE_CLASS),$(JAVA_SDK_ENFORCEMENT_ERROR))
+#        $(call pretty-error,$(my_message))
+#      endif
+#      ifeq ($(LOCAL_MODULE_CLASS),$(JAVA_SDK_ENFORCEMENT_WARNING))
+#        $(call pretty-warning,$(my_message))
+#      endif
+#      my_message :=
+#    endif
+#  endif
+#else ifneq ($(LOCAL_SDK_VERSION),)
+#  ifneq ($(LOCAL_PRIVATE_PLATFORM_APIS),)
+#    my_message := Specifies both LOCAL_SDK_VERSION ($(LOCAL_SDK_VERSION)) and
+#    my_message += LOCAL_PRIVATE_PLATFORM_APIS ($(LOCAL_PRIVATE_PLATFORM_APIS))
+#    my_message += but should specify only one
+#    $(call pretty-error,$(my_message))
+#    my_message :=
+#  endif
 endif
