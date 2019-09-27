2602e54050901c777fabce9bf37b76b7c4ca48e1
Marko Man <darkobas@gmail.com>
2019-09-27 22:06:30 +0200

    move back soong flags
    Change-Id: Icec518552c5ef6555787ba5dbc78b95597ea0429

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -168,6 +168,17 @@ $(call add_json_list, ProductHiddenAPIStubsTest,         $(PRODUCT_HIDDENAPI_STU
 
 $(call add_json_list, TargetFSConfigGen,                 $(TARGET_FS_CONFIG_GEN))
 
+#Omni
+$(call add_json_bool, Healthd_use_battery_info,          $(filter true,$(HEALTHD_USE_BATTERY_INFO)))
+$(call add_json_bool, Healthd_enable_op_fastchg,         $(filter true,$(HEALTHD_ENABLE_OP_FASTCHG_CHECK)))
+$(call add_json_bool, TargetNeedsHWCOnFirstRef,          $(filter true,$(TARGET_NEEDS_HWC_ONFIRSTREF)))
+$(call add_json_bool, Uses_metadata_as_fde_key,          $(filter true,$(TARGET_USES_METADATA_AS_FDE_KEY)))
+$(call add_json_bool, Target_use_sdclang,                $(filter true,$(TARGET_USE_SDCLANG)))
+$(call add_json_bool, Target_camera_needs_client_info,   $(filter true,$(TARGET_CAMERA_NEEDS_CLIENT_INFO)))
+$(call add_json_bool, Target_motorized_camera,           $(filter true,$(TARGET_MOTORIZED_CAMERA)))
+$(call add_json_str_omitempty, Target_init_vendor_lib,   $(TARGET_INIT_VENDOR_LIB))
+
+
 $(call add_json_map, VendorVars)
 $(foreach namespace,$(SOONG_CONFIG_NAMESPACES),\
   $(call add_json_map, $(namespace))\
