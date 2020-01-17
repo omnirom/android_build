c7c21241ad50bd806f7deafc6cef0d69fa57a1dd
Marko Man <darkobas@gmail.com>
2020-01-17 01:35:17 +0100

    Build: build release if we say so
    Change-Id: I51866385b6e2d658f3b21821597c0dbf34a76c6b
    
    build: add MICROG to release-keys filter
    
    Change-Id: I2095a211141cf8107858ba62a48724b86dac5f4b

--- core/sysprop.mk
+++ core/sysprop.mk
@@ -149,10 +149,14 @@ endif
 # non-default dev keys (usually private keys from a vendor directory).
 # Both of these tags will be removed and replaced with "release-keys"
 # when the target-files is signed in a post-build step.
+ifneq ($(filter $(ROM_BUILDTYPE), WEEKLY GAPPS MICROG),)
+BUILD_KEYS := release-keys
+else
 ifeq ($(DEFAULT_SYSTEM_DEV_CERTIFICATE),build/make/target/product/security/testkey)
 BUILD_KEYS := test-keys
 else
 BUILD_KEYS := dev-keys
+endif
 endif
 BUILD_VERSION_TAGS += $(BUILD_KEYS)
 BUILD_VERSION_TAGS := $(subst $(space),$(comma),$(sort $(BUILD_VERSION_TAGS)))
