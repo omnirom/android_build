20f75162938a2fae1900e692e6fd9ebf6fad113d
Menno van Grinsven <scanno71@gmail.com>
2020-02-20 15:31:20 +0100

    build: check the fstab for the system_root mountpoint
    Change-Id: I8045e0c700d9e499601a5c495410aa23a51e49ca

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -831,7 +831,12 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("#######################################");
 
   if target_info.get("system_root_image") == "true":
-    sysmount = "/"
+    if script.fstab["/system_root"]:
+       script.Print("system mounts on /system_root");
+       sysmount = "/system_root"
+    else:
+       script.Print("system mounts on /");
+       sysmount = "/"
   else:
     sysmount = "/system"
 
