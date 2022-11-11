c0c63f19e41166619f46f4bde9609cf4e35f9e9f
maxwen <max.weninger@gmail.com>
2022-11-11 22:10:42 +0100

    make: dont add device/sample/etc/apns-full-conf
    Change-Id: I59fa41d15b2b11057d98488c18caa3cd0dd90d7e

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -36,5 +36,5 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
