1fa6cb9b779df136a7f31c2d86e9824fb340ba58
Bill Yang <yangbill@google.com>
2024-09-20 03:13:58 +0000

    Export ril related flag to soong
    Export SIM_COUNT, DISABLE_RILD_OEM_HOOK and ENABLE_VENDOR_RIL_SERVICE
    to soong
    
    Bug: 347679222
    Test: m libril libreference-ril rild
    Change-Id: Ic1f8262f260c3fec70635809acf80d340502a77c

--- core/android_soong_config_vars.mk
+++ core/android_soong_config_vars.mk
@@ -201,3 +201,12 @@ $(call soong_config_set_bool,gralloc,target_use_pan_display,$(if $(filter true,$
 
 # Add use_camera_v4l2_hal flag for hardware/libhardware/modules/camera/3_4:camera.v4l2
 $(call soong_config_set_bool,camera,use_camera_v4l2_hal,$(if $(filter true,$(USE_CAMERA_V4L2_HAL)),true,false))
+
+# Add sim_count, disable_rild_oem_hook, and use_aosp_rild flag for ril related modules
+$(call soong_config_set,ril,sim_count,$(SIM_COUNT))
+ifneq ($(DISABLE_RILD_OEM_HOOK), false)
+  $(call soong_config_set_bool,ril,disable_rild_oem_hook,true)
+endif
+ifneq ($(ENABLE_VENDOR_RIL_SERVICE), true)
+  $(call soong_config_set_bool,ril,use_aosp_rild,true)
+endif
